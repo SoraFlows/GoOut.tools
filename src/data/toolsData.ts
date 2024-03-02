c8bf30cf4faa044a1281cd44675bcb3a790c790e
RongkangXiong <earth@mail.ustc.edu.cn>
2024-03-02 17:20:23 +0800

    chore: del 无用组件

--- src/data/toolsData.ts
+++
@@ -1,6 +0,0 @@
-import { CategoryData } from "@/types/ToolData";
-import toolsDataJson from "@/data/tools.json";
-
-// 使用类型断言确保JSON数据符合CategoryData数组类型
-// 调整类型断言来匹配JSON结构
-export const toolsData: CategoryData[] = toolsDataJson.tools as CategoryData[];
